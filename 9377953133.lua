419680d252db476cd94a9d0cf3cfc3fa5ec9beff
Fuzeuchi17 <lhun28305@gmail.com>
2025-07-11 18:45:23 -0500

    Create 9377953133.lua

---
+++ 9377953133.lua
@@ -0,0 +1,10 @@
+getgenv().TempestHubVersion = "v1.4.4"
+
+local supportedGames = {
+"9377953133" -- MSF
+}
+
+if table.find(supportedGames, tostring(game.GameId)) then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/10tempest01/tempest-hub/refs/heads/main/Games/" .. game.GameId .. ".lua"))()
+end
+
