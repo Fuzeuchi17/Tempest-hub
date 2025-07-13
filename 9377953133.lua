ee8270e393ca49feca0e2a0b270778265c312e54
Fuzeuchi17 <lhun28305@gmail.com>
2025-07-13 18:10:38 -0500

    Update 9377953133.lua

--- 9377953133.lua
+++ 9377953133.lua
@@ -1,10 +1,11 @@
 getgenv().TempestHubVersion = "v1.4.4"
 
 local supportedGames = {
-"9377953133" -- MSF
+    "9377953133" -- MSF
 }
 
 if table.find(supportedGames, tostring(game.GameId)) then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/10tempest01/tempest-hub/refs/heads/main/Games/" .. game.GameId .. ".lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fuzeuchi17/tempest-hub/main/" .. game.GameId .. ".lua"))()
+else
+    warn("Game not supported: " .. tostring(game.GameId))
 end
-
