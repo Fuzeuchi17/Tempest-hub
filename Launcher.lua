0b8b318c24376c7e48d198ea961eec3fde546063
Fuzeuchi17 <lhun28305@gmail.com>
2025-07-13 18:28:29 -0500

    Update Launcher.lua

--- Launcher.lua
+++ Launcher.lua
@@ -1,11 +1,11 @@
 getgenv().TempestHubVersion = "v1.4.4"
 
-local supportedGames = {
-    "9377953133" -- Your Game: MSF
+local supportedPlaces = {
+    9377953133 -- Mobile Sword Fight
 }
 
-if table.find(supportedGames, tostring(game.GameId)) then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fuzeuchi17/tempest-hub/main/" .. game.GameId .. ".lua"))()
+if table.find(supportedPlaces, game.PlaceId) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fuzeuchi17/tempest-hub/main/" .. game.PlaceId .. ".lua"))()
 else
-    warn("TempestHub: Game not supported. GameId = " .. tostring(game.GameId))
+    warn("TempestHub: Place not supported. PlaceId = " .. tostring(game.PlaceId))
 end
