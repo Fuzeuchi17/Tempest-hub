f04ddfc222709fc0eb25cd24d7f0f878fb5d6ba1
Fuzeuchi17 <lhun28305@gmail.com>
2025-07-14 04:41:26 -0500

    Update 3504161197.lua

--- 3504161197.lua
+++ 3504161197.lua
@@ -5,7 +5,5 @@ local supportedGames = {
 }
 
 if table.find(supportedGames, tostring(game.GameId)) then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/10tempest01/tempest-hub/main/Games/" .. game.GameId .. ".lua"))()
-else
-    warn("❌ Game not supported.")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/10tempest01/tempest-hub/refs/heads/main/Games/" .. game.GameId .. ".lua"))()
 end
