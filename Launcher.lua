5c6d8af1b2bcf9b28b1380e11f672a8c48c1cc58
Fuzeuchi17 <lhun28305@gmail.com>
2025-07-14 03:57:11 -0500

    Update Launcher.lua

--- Launcher.lua
+++ Launcher.lua
@@ -1,11 +1,11 @@
 getgenv().TempestHubVersion = "v1.4.4"
 
 local supportedGames = {
-    "3504161197" -- MSF or your current game's GameId
+    "3504161197" -- MSF (Correct GameId)
 }
 
 if table.find(supportedGames, tostring(game.GameId)) then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Fuzeuchi17/tempest-hub/main/Games/" .. game.GameId .. ".lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/10tempest01/tempest-hub/refs/heads/main/Games/" .. game.GameId .. ".lua"))()
 else
     warn("Game not supported: " .. tostring(game.GameId))
 end
